663c94008bad389ee31a36d0603c3cc65f720630
MjRodge <michael@1mrkt.to>
2023-02-08 16:50:08 -0500

    created db model and migration

--- polling/settings.py
+++ polling/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polling_app.apps.PollingAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

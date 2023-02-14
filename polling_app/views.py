85c6df89222403cafd6dfa99b5d4f5fb212422ec
MjRodge <michael@1mrkt.to>
2023-02-14 11:01:01 -0500

    created initial voting views

--- polling_app/views.py
+++ polling_app/views.py
@@ -5,3 +5,17 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, World. You are at the polling index.")
+
+
+def detail(request, question_id):
+    return HttpResponse("You're viewing question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're viewing the results of question %s"
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s" % question_id)
+
